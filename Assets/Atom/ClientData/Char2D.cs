3041294fc6372255d3d9a928bad6d052896d3735
Rickman Roedavan <rikman@telkomuniversity.ac.id>
2020-08-11 15:09:16 +0700

    4 Character

---
+++ Assets/Atom/ClientData/Char2D.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CharacterCreator2D
+{
+    public class Char2D : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        public CharacterViewer TargetCharacter;
+        public Color ShowColor;
+        public Color HideColor;
+
+        public void ShowCharacter()
+        {
+            TargetCharacter.TintColor = ShowColor;
+            TargetCharacter.RepaintTintColor();
+        }
+        public void HideCharacter()
+        {
+            TargetCharacter.TintColor = HideColor;
+            TargetCharacter.RepaintTintColor();
+        }
+
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
